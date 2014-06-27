5c72f1d35241f576640036a1a6d51c9ed6b1f0c4
Bhagat Bandlamudi <bhagat.bandlamudi@marklogic.com>
2014-06-27 18:59:30 -0400

    fixed issues with synchronization

--- src/java/com/marklogic/developer/corb/ExportBatchToFileTask.java
+++ src/java/com/marklogic/developer/corb/ExportBatchToFileTask.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import com.marklogic.xcc.ResultSequence;
 
 public class ExportBatchToFileTask extends ExportToFileTask {
+	static private Object sync = new Object();
 	
 	protected String getFileName(){
 		String fileName = getProperty("EXPORT-FILE-NAME");
@@ -23,19 +24,20 @@ public class ExportBatchToFileTask extends ExportToFileTask {
 		return fileName;
 	}
 	
-	synchronized protected void writeToFile(String fileName, ResultSequence seq) throws IOException{
+	protected void writeToFile(String fileName, ResultSequence seq) throws IOException{
 		if(!seq.hasNext()) return;
-		BufferedOutputStream writer = null;
-		try{
-			writer = new BufferedOutputStream(new FileOutputStream(new File(exportDir,getFileName()),true));
-			writer.write(getValueAsBytes(seq.next().getItem()));
-			while(seq.hasNext()){
-				writer.write(NEWLINE);
-				writer.write(getValueAsBytes(seq.next().getItem()));
-			}
-		}finally{
-			if(writer != null){
-				writer.close();
+		synchronized(sync){
+			BufferedOutputStream writer = null;
+			try{
+				writer = new BufferedOutputStream(new FileOutputStream(new File(exportDir,getFileName()),true));
+				while(seq.hasNext()){
+					writer.write(getValueAsBytes(seq.next().getItem()));
+					writer.write(NEWLINE);
+				}
+			}finally{
+				if(writer != null){
+					writer.close();
+				}
 			}
 		}
 	}
